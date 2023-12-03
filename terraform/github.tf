67fcd72afd3f87824a482374a6da40f2fe2de540
James Geddes <j@jamesgeddes.pro>
2023-12-03 14:53:46 +0000

    manage org members from json file

---
+++ terraform/github.tf
@@ -0,0 +1,9 @@
+# NB!
+# This is a ***TEMPORARY*** solution
+# Once Geek.Zone/Web can manage groups, this will no longer be necessary.
+
+resource "github_membership" "all" {
+  for_each = local.github_usernames_roles
+  username = each.key
+  role     = each.value
+}
